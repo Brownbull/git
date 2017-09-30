d00240b572dbd73fd8366b91be56b2993e97d1d9
brownbull <carcamo.gabriel@gmail.com>
2017-09-30 20:25:02 -0300

    added SSH Fix file

---
+++ GitSSHFix.sh
@@ -0,0 +1,9 @@
+# Fix ssh for git
+# Create public key 
+# Open gitbash
+$ ssh -keygen
+enter 
+enter
+# go to folder where the key was created
+$ cd C:\Users\${Your_User}\.ssh
+# Open id_rsa.pub file copy key and add it to your Git Keys
